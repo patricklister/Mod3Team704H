e0863d412f26c0945bebfd5c8bd10ca671fb40be
cameroon25 <cameroon25@users.noreply.github.com>
2020-03-10 03:00:12 -0400

    Update basic_alg.py

--- basic_alg.py
+++ basic_alg.py
@@ -4,6 +4,7 @@
 import rospy  # this is the module required for all simulation communication
 import math
 import time
+import datetime
 
 # start of wheel control code
 from wheel_control.msg import wheelSpeed  # this is a required module for the drive communication
@@ -81,41 +82,87 @@ class Rover:
         self.locationHeadingObj = locationHeadingObj
         self.wheelControlObj = wheelControlObj
 
-        turnErrorThreshhold = 0.1
-        turnIncrement = 1
-        lastTurnDirection = 0
+        self.turnErrorThreshhold = 0.01
+
+    def face_goal(self):
+        deltaX = self.destCoor[0] - self.locationHeadingObj.x
+        deltaY = self.destCoor[1] - self.locationHeadingObj.y
+
+        if deltaX > 0 and deltaY > 0:         #quad 1
+            RAA = math.atan(abs(deltaY)/abs(deltaX))
+            angle = RAA
+
+        elif deltaX < 0 and deltaY > 0:         #quad 2
+            RAA = math.atan(abs(deltaY)/abs(deltaX))
+            angle = math.pi - RAA
+            
+        elif deltaX < 0 and deltaY < 0:         #quad 3
+            RAA = math.atan(abs(deltaY)/abs(deltaX))
+            angle = math.pi + RAA
+
+        elif deltaX > 0 and deltaY < 0:         #quad 4
+            RAA = math.atan(abs(deltaY)/abs(deltaX))
+            angle = 2*(math.pi) - RAA
+
+        elif deltaX == 0 and deltaY > 0:    #goal above it
+            angle = (math.pi)/2   #90 deg from postive x-axis
+        
+        elif deltaX == 0 and deltaY < 0:   #goal below it
+            angle = (3*(math.pi))/2   #270 deg from postive x-axis
+
+        elif deltaX > 0 and deltaY == 0:   #goal to right of it
+            angle = 0
+
+        elif deltaX < 0 and deltaY == 0:    #goal to left of it
+            angle = math.pi     #180 deg from postive x-axis
+
+        elif deltaX == 0 and deltaY == 0:
+            return True     #YOU ALREADY AT GOAL
+
+        maxAngle = angle + self.turnErrorThreshhold
+        minAngle = angle - self.turnErrorThreshhold
+
+        print(RAA)
+        print(angle)
+        print(self.locationHeadingObj.heading)
+        print(str(minAngle) + " - " + str(maxAngle))
+
+        while self.locationHeadingObj.heading > maxAngle or self.locationHeadingObj.heading < minAngle:
+            print("HEY!")
+            self.wheelControlObj.drive_wheels(-0.5, 0.5)
+            print(self.locationHeadingObj.heading)
+            print(str(minAngle) + " - " + str(maxAngle))
 
 
-    def go_straight (self, distance, forOrBack):
+
+    def go_straight_dist (self, distance, forOrBack):
         initCoor = [self.locationHeadingObj.x, self.locationHeadingObj.y]
 
         distanceTravelled = 0
 
         speed = 0.25
-        
+
         while distanceTravelled <= distance:
+
             if forOrBack == "f":
                 velocity = speed
             elif forOrBack == "b":
                 velocity = (-1)*speed
 
+
             self.wheelControlObj.drive_wheels(velocity, velocity)
 
             newCoor = [self.locationHeadingObj.x, self.locationHeadingObj.y]
+            
+
             distanceTravelled = ((newCoor[0]-initCoor[0])**2 + (newCoor[1]-initCoor[1])**2)**(0.5)
+
 
             print(str(initCoor) + "   " + str(newCoor))
             print(str(distanceTravelled) + ">" + str(distance))
 
-            if distanceTravelled >= 0.98*distance:
-                speed = 0.001
-            elif distanceTravelled >= 0.95*distance:
-                speed = 0.025
-            elif distanceTravelled >= 0.90*distance:
-                speed = 0.05
-            elif distanceTravelled >= 0.80*distance:
-                speed = 0.1
-            
+
+
 
         self.wheelControlObj.drive_wheels(0,0)
 
@@ -127,6 +174,39 @@ class Rover:
         print(distanceTravelled)
 
 
+
+    def rotate(self, desiredAngle):
+        if desiredAngle < 90:
+            desiredHeading = self.locationHeadingObj.heading - (desiredAngle*((math.pi)/180))
+            rw = 0.5
+            lw = -0.5
+
+        elif desiredAngle == 90:
+            desiredHeading = self.locationHeadingObj.heading
+            return True
+
+        elif desiredAngle > 90:
+            desiredHeading = self.locationHeadingObj.heading + ((desiredAngle-90)*((math.pi)/180))
+            rw = -0.5
+            lw = 0.5
+
+        print(self.locationHeadingObj.heading)
+        maxHeading = desiredHeading + self.turnErrorThreshhold
+        minHeading = desiredHeading - self.turnErrorThreshhold
+
+        print(desiredHeading)
+        print(str(minHeading) + " - " + str(maxHeading))             
+        
+        while self.locationHeadingObj.heading < minHeading or self.locationHeadingObj.heading > maxHeading:
+            self.wheelControlObj.drive_wheels(rw, lw)
+
+        print("***")
+        print(self.locationHeadingObj.heading)
+
+        self.wheelControlObj.drive_wheels(0, 0)
+
+    
+    """
     # pass in a magnitude and heading to get the desired movement vector
     # Add this vector to the current coordinates to get the desired coordinates
     def get_desired_vector (self, magnitude, heading):
@@ -135,7 +215,7 @@ class Rover:
 
     def rotate (self, desiredHeading):
         # Get the rover's current heading
-        currentHeading = locationHeadingObj.heading
+        currentHeading = self.locationHeadingObj.heading
 
         # If the current heading is in the desired direction within error, return True
         if (abs(currentHeading - desiredHeading) <= self.turnErrorThreshhold):
@@ -150,12 +230,12 @@ class Rover:
         if currentTurnDirection != lastTurnDirection and lastTurnDirection != 0:
             turnIncrement /= 2
 
-        wheelControlObj.drivewheels(-1 * currentTurnDirection * turnIncrement, currentTurnDirection * turnIncrement)
+        self.wheelControlObj.drivewheels(-1 * currentTurnDirection * turnIncrement, currentTurnDirection * turnIncrement)
 
         lastTurnDirection = currentTurnDirection
 
         return False
-
+    """
         
         
 
@@ -165,19 +245,42 @@ locHead  = LocationHeading()
 laser = LaserListener()
 wheel = WheelController()
 
-roverObj = Rover([0,0], locHead, wheel)
+xDestCoor = float(input("Enter goal coordinates below...\nX Coordinate: "))
+yDestCoor = float(input("Y Coordinate: "))
+print("\n")
+
+roverObj = Rover([xDestCoor, yDestCoor], locHead, wheel)
 #end of initialization
+
+#####ENSURE ROVER IS AT Z AXIS OF ZERO OR ELSE IT FLIPS OUT
 
 
 # start of control loop snippet
 
+
 while not rospy.is_shutdown():  #this will run until gazebo is shut down or CTRL+C is pressed in the ubuntu window that is running this code
+
+    wheel.drive_wheels(0, 0)
+    time.sleep(1)
+    
+    print("Loading...")
     time.sleep(5)
 
-    roverObj.go_straight(10, "b")
+    roverObj.face_goal()
 
+    """
+    #roverObj.go_straight()
+    print(laser.laserRanges)
+
+    nearestObjectCentre = (laser.laserRanges[7] + laser.laserRanges[8])/2
+
+    if nearestObjectCentre > 2:
+        print("TIME TO TURN")
+    else:
+        wheel.drive_wheels(1,1)
+        #roverObj.go_straight("f")
+    """
     
-    break
 
     """
     minRange = 99 #initialize minRange to a value larger than what will be recieved
