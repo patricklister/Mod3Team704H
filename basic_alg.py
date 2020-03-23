69943e736afd762fd15dc433d9b257a51ccca50a
cameroon25 <cameroon25@users.noreply.github.com>
2020-03-23 14:29:06 -0400

    Update basic_alg.py

--- basic_alg.py
+++ basic_alg.py
@@ -87,10 +87,13 @@ class Rover:
     def face_goal(self):
         deltaX = self.destCoor[0] - self.locationHeadingObj.x
         deltaY = self.destCoor[1] - self.locationHeadingObj.y
+        RAA = 0
+
 
         if deltaX > 0 and deltaY > 0:         #quad 1
             RAA = math.atan(abs(deltaY)/abs(deltaX))
             angle = RAA
+
 
         elif deltaX < 0 and deltaY > 0:         #quad 2
             RAA = math.atan(abs(deltaY)/abs(deltaX))
@@ -119,6 +122,30 @@ class Rover:
         elif deltaX == 0 and deltaY == 0:
             return True     #YOU ALREADY AT GOAL
 
+
+
+        direction = 0 #1 is CW, -1 is CCW
+        if self.locationHeadingObj.heading < angle:
+            if abs(self.locationHeadingObj.heading - angle) < 180:
+                direction = -1
+            else:
+                direction = 1
+
+        else:
+            if abs(self.locationHeadingObj.heading - angle) < 180:
+                direction = 1
+            else:
+                direction = -1
+
+        if direction == 1:  #CW
+            lw = 0.7
+            rw = -0.7
+        else:               #CCW
+            lw = -0.7
+            rw = 0.7
+
+        print(direction)
+
         maxAngle = angle + self.turnErrorThreshhold
         minAngle = angle - self.turnErrorThreshhold
 
@@ -127,22 +154,17 @@ class Rover:
         print(self.locationHeadingObj.heading)
         print(str(minAngle) + " - " + str(maxAngle))
 
-        diff = angle - self.locationHeadingObj.heading
-        if diff <= math.pi:
-            lw = -0.6
-            rw = 0.6
-        elif diff > math.pi:
-            lw = 0.6
-            rw = -0.6
-
         
-        while self.locationHeadingObj.heading > maxAngle or self.locationHeadingObj.heading < minAngle:
+        while (self.locationHeadingObj.heading > maxAngle or self.locationHeadingObj.heading < minAngle) and not rospy.is_shutdown():
             self.wheelControlObj.drive_wheels(lw, rw)
 
+            print("h")
             """
             print(self.locationHeadingObj.heading)
             print(str(minAngle) + " - " + str(maxAngle))
             """
+        
+        self.wheelControlObj.drive_wheels(0,0)
 
 
 
@@ -154,7 +176,7 @@ class Rover:
 
         speed = 0.25
 
-        while distanceTravelled <= distance and not rospy.is_shutdown():
+        while (distanceTravelled <= distance) and not rospy.is_shutdown():
 
             if forOrBack == "f":
                 velocity = speed
@@ -169,10 +191,10 @@ class Rover:
 
             distanceTravelled = ((newCoor[0]-initCoor[0])**2 + (newCoor[1]-initCoor[1])**2)**(0.5)
 
-
+            """
             print(str(initCoor) + "   " + str(newCoor))
             print(str(distanceTravelled) + ">" + str(distance))
-
+            """
 
 
 
@@ -187,11 +209,17 @@ class Rover:
 
 
 
+    def calc_angle(self, laserRangeIndex):
+        angle = (math.pi/4) + (math.pi/2)*laserRangeIndex
+
+        return angle
+
+
     def rotate(self, desiredAngle):
         if desiredAngle < 90:
             desiredHeading = self.locationHeadingObj.heading - (desiredAngle*((math.pi)/180))
-            lw = 0.5
-            rw = -0.5
+            lw = 1
+            rw = -1
 
         elif desiredAngle == 90:
             desiredHeading = self.locationHeadingObj.heading
@@ -199,8 +227,8 @@ class Rover:
 
         elif desiredAngle > 90:
             desiredHeading = self.locationHeadingObj.heading + ((desiredAngle-90)*((math.pi)/180))
-            lw = -0.5
-            rw = 0.5
+            lw = -1
+            rw = 1
 
         print(self.locationHeadingObj.heading)
         maxHeading = desiredHeading + self.turnErrorThreshhold
@@ -209,7 +237,6 @@ class Rover:
         print(desiredHeading)
         print(str(minHeading) + " - " + str(maxHeading))             
         
-
         while (self.locationHeadingObj.heading < minHeading or self.locationHeadingObj.heading > maxHeading) and not rospy.is_shutdown():
             self.wheelControlObj.drive_wheels(lw, rw)
 
@@ -217,6 +244,44 @@ class Rover:
         print(self.locationHeadingObj.heading)
 
         self.wheelControlObj.drive_wheels(0, 0)
+
+
+
+    def get_xy(self, laserRanges):
+        outputList = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+        comparisonValue = laserRanges[0]
+        currentAngle = math.pi/4
+        for i in range(16):
+            currentAngle = (math.pi/4) + (math.pi/30)*i
+
+            if laserRanges[i] == float("inf"):
+                newListItem = [float("inf"), float("inf")]
+
+            else:
+                dist = laserRanges[i]
+
+                if currentAngle > (math.pi/2):
+                    tempAngle = (math.pi) - currentAngle
+                    xComponent = (abs(dist*(math.cos(tempAngle))))*-1
+                    yComponent = abs(dist*(math.sin(tempAngle)))
+
+                else:
+                    xComponent = abs(dist*(math.cos(currentAngle)))
+                    yComponent = abs(dist*(math.sin(currentAngle)))
+
+                newListItem = [xComponent, yComponent]
+
+            #print(i, currentAngle)
+            
+            outputList[i] = newListItem
+            
+        
+        return outputList
+
+
+            
+
 
     
     """
@@ -258,11 +323,16 @@ locHead  = LocationHeading()
 laser = LaserListener()
 wheel = WheelController()
 
+time.sleep(2)
+    
+wheel.drive_wheels(0, 0)
+
 xDestCoor = float(input("Enter goal coordinates below...\nX Coordinate: "))
 yDestCoor = float(input("Y Coordinate: "))
 print("\n")
 
 roverObj = Rover([xDestCoor, yDestCoor], locHead, wheel)
+
 #end of initialization
 
 #####ENSURE ROVER IS AT Z AXIS OF ZERO OR ELSE IT FLIPS OUT
@@ -270,28 +340,207 @@ roverObj = Rover([xDestCoor, yDestCoor], locHead, wheel)
 
 # start of control loop snippet
 
+print("Loading...")
+time.sleep(3)
+
+roverObj.face_goal()
 
 while not rospy.is_shutdown():  #this will run until gazebo is shut down or CTRL+C is pressed in the ubuntu window that is running this code
-
-    wheel.drive_wheels(0, 0)
-    time.sleep(1)
-    
-    print("Loading...")
-    time.sleep(5)
+    roverObj.wheelControlObj.drive_wheels(0,0)
 
     roverObj.face_goal()
 
-    """
-    #roverObj.go_straight()
-    print(laser.laserRanges)
+    laserValues= laser.laserRanges
+    laserCoor = roverObj.get_xy(laserValues)
 
-    nearestObjectCentre = (laser.laserRanges[7] + laser.laserRanges[8])/2
+    possiblePath = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] #1 means no, 0 means yes it is clear
+    zeroCount = 0
+    oneCount = 16
+    infCount = 0
+    for i in range(16):
+        if laserValues[i] == float("inf") or laserValues[i] > 3:
+            possiblePath[i] = 0
+            zeroCount += 1
+            oneCount -= 1
 
-    if nearestObjectCentre > 2:
-        print("TIME TO TURN")
+        if laserValues[i] == float("inf"):
+            infCount += 1
+
+    print("***************")
+    print(oneCount)
+
+    if zeroCount != 16:
+
+        if oneCount == 0:
+            go_straight_dist(1, "b")
+
+        else:
+
+            print(laserValues)
+            print(possiblePath)
+
+            pathCount = 0
+            bestPathCount = 0
+            minIndexBest = 0
+            maxIndexBest = 0
+            for i in range(16):
+                if possiblePath[i] == 0 and i != 15:
+                    pathCount += 1
+                else:
+                    if i != 0:
+                        deltaX = abs(laserCoor[i-1][0] - laserCoor[i-pathCount][0])
+                        deltaY = abs(laserCoor[i-1][0] - laserCoor[i-pathCount][0])
+
+                        
+                        print("dx: " + str(deltaX) + ", pathCount: " + str(pathCount))
+                        
+                        openSpace = False
+                        if not(deltaX == float("nan")):
+                            openSpace = True
+
+                        if deltaX > 2 or openSpace:
+                            for j in range(pathCount):
+                                possiblePath[(i-1)-j] = 0
+
+                        else:
+                            for j in range(pathCount):
+                                possiblePath[(i-1)-j] = 1
+                        
+                        if bestPathCount < pathCount:
+                            bestPathCount = pathCount
+                            maxIndexBest = i
+                            minIndexBest = (i-1)-pathCount
+
+                        pathCount = 0
+            
+            print(possiblePath)
+
+            print("-------")
+
+            idealIndexDirection = round(((maxIndexBest+minIndexBest)/2),2)
+
+            print(idealIndexDirection)
+
+            angle = roverObj.calc_angle(idealIndexDirection)
+            
+            time.sleep(1)
+            roverObj.rotate(angle)
+            time.sleep(1)
+
+            roverObj.go_straight_dist((laserValues[minIndexBest]+laserValues[maxIndexBest])/2, "f")
+
+
     else:
-        wheel.drive_wheels(1,1)
-        #roverObj.go_straight("f")
+        roverObj.wheelControlObj.drive_wheels(1, 1)
+
+        time.sleep(0.5)
+    
+    roverObj.wheelControlObj.drive_wheels(0, 0)
+
+    time.sleep(5)
+
+
+    # lastX = 0
+    # for i in range(16):
+    #     if (i+1) <= 15:
+    #         if laserValues[i] == float("inf"):
+    #             lastX = 0
+    #         else:
+                
+                
+    #             if laserValues[i+1] == float("inf"):
+                    
+    #             deltaX = laserValues[i+1] - laserValues[i]
+                
+    #             if deltaX > 1.5:
+
+
+
+
+    
+
+
+    """
+    wheel.drive_wheels(0,0)
+
+    roverObj.face_goal()
+
+    currentLaser = laser.laserRanges
+    print(currentLaser)
+    print(type(currentLaser[0]))
+
+    isPathClear = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] #1 means no, 0 means yes it is clear
+
+    nearestObjectCentre = (currentLaser[7] + currentLaser[8])/2
+
+    adjust = False
+    for i in currentLaser:
+        if i <= 5 and i != float('inf'):
+            adjust = True
+            break
+
+
+    distanceToGoal = ((xDestCoor-roverObj.locationHeadingObj.x)**2 + (yDestCoor-roverObj.locationHeadingObj.y)**2)**(0.5)
+    if 
+
+
+
+
+
+    if adjust:
+        indexL = 6
+        deviationsL = 0
+        while indexL >= 0:
+            diff = currentLaser[indexL] - nearestObjectCentre
+            if diff > 1:
+                isPathClear[indexL] = 0
+                break
+            else:
+                deviationsL += 1
+
+            indexL -= 1
+            
+        indexR = 9
+        deviationsR = 0
+        while indexR <= 15:
+            diff = currentLaser[indexR] - nearestObjectCentre
+            if diff > 1:
+                isPathClear[indexR] = 0
+                break
+            else:
+                deviationsR += 1
+
+            indexR += 1
+
+        angle = 0
+        if deviationsL < deviationsR:
+            angle = roverObj.calc_angle(indexL)
+        elif deviationsR < deviationsL:
+            angle = roverObj.calc_angle(indexR)
+        
+
+        time.sleep(5)
+
+        if angle != 0:
+            roverObj.rotate(angle)
+
+            roverObj.go_straight_dist(nearestObjectCentre, "f")
+
+        else:
+            roverObj.rotate(math.pi/2)
+            roverObj.go_straight_dist(2, "f")
+
+        
+        roverObj.face_goal()
+
+
+
+    else:
+        wheel.drive_wheels(0.5, 0.5)
+
+    time.sleep(0.01)
+
+            
     """
     
 
